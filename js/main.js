802dc3951d78a8bbc8c06383e7b51533e3beb3a9
AlexandruChiriac98 <achiriac98@gmail.com>
2022-11-22 09:28:39 +0100

    finito

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Trentatre trentini andarono a Trento tutti e trentratre trotterellando',
+        }
+    }
+}).mount("#app")
